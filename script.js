1967861826cd19d1b7e733ccdbae6d7ce0865d32
Nhlamulo <nhlamulob7@gmail.com>
2026-08-10 19:46:24 +0200

    Update guessing range display and enhance game logic for number guessing game

--- script.js
+++ script.js
@@ -8,26 +8,28 @@ const wins = document.getElementById('wins');
 
 
 let isWin = false;
-let count = 0;
+let attempted = 0;
 let winnigs = 0;
 
 let getRange = []
+let targetValue = []
 
 function generateRange() {
     let randomizeNumbers = Math.floor(Math.random() * 99 + 1);
     getRange = [randomizeNumbers];
 
-    range.textContent = `Guessing range: 0 - ${randomizeNumbers}`;
+    range.textContent = `Number is between: ${Math.ceil(randomizeNumbers - 7)} - ${randomizeNumbers + 7}`;
     console.log(randomizeNumbers);
     console.log(getRange);
 }
 
-generateRange();
 
-submitBtn.disabled = false;
+
+submitBtn.disabled = true;
 
 userValue.addEventListener('input', (e) => {
     const value = e.target.value;
+    targetValue = [value];
 
     if (value.length > 0 && !isNaN(value)) {
         submitBtn.disabled = false;
@@ -37,11 +39,40 @@ userValue.addEventListener('input', (e) => {
     }
 });
 
+userValue.addEventListener('click', function(){
+    generateRange();
+});
+
 submitBtn.addEventListener('click', () => {
-    let randomizeNumbers = Math.floor(Math.random() * 99 + 1);
-    getRange = [randomizeNumbers];
+    attempted++
+    const clearField = document.getElementById('userValue');
+    const getRangeValue = getRange[0];
+    const user = userValue.value;
+    console.log(typeof num);
 
+    const getTargetValue = targetValue[0];
 
-    
-    randomNum.textContent = randomizeNumbers;
+    if (Number(getRangeValue) === Number(getTargetValue)) {
+        isWin = true;
+        alert("You got it");
+    }
+
+    if (Number(getRangeValue) !== Number(getTargetValue)) {
+        isWin = false;
+    }
+
+    if (isWin === true) {
+        winnigs++
+        wins.textContent = `Wins: ${winnigs}`;
+    }
+
+    if (isWin === false) {
+        wins.textContent = `Wins: ${winnigs}`;
+    }
+
+    randomNum.textContent = getRange[0];
+    attempts.textContent = `Attempts: ${attempted}`;
+    chosenNum.textContent = ` Chosen no: ${targetValue[0]}`;
+
+    clearField.value = '';
 });
